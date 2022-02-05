cd86bf3208469a92c0f8e512ebb6b628c9a1e045
Gaurav Jain <meetgauravjain@gmail.com>
2022-02-05 16:31:48 -0700

    Adding common response opbect

--- src/main/java/com/gj/rest/webservices/restfulwebservices/Exceptions/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/gj/rest/webservices/restfulwebservices/Exceptions/CustomizedResponseEntityExceptionHandler.java
@@ -13,13 +13,10 @@ import java.util.Date;
 @ControllerAdvice
 @RestController
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
-
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
                 request.getDescription(false));
             return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
-
-
 }
