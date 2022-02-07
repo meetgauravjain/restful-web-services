26901d7ff14cb32a993ca5a7e7b092c15d366b46
Gaurav Jain <meetgauravjain@gmail.com>
2022-02-07 08:04:32 -0700

    adding jpa

---
+++ src/main/java/com/gj/rest/webservices/restfulwebservices/Beans/Post.java
@@ -0,0 +1,32 @@
+package com.gj.rest.webservices.restfulwebservices.Beans;
+
+public class Post {
+
+    private Integer id;
+    private String description;
+    private User user;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
